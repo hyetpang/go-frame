19bc1d8a083402a10d0c4cf61eda1944df9fc29f
guangming.zhang <hyetpang@yeah.net>
2022-06-17 14:32:46 +0800

    ✨ feat: 增加nanoid生成的函数

--- pkgs/common/nano_id_gen.go
+++ pkgs/common/nano_id_gen.go
@@ -3,5 +3,16 @@ package common
 import gonanoid "github.com/matoous/go-nanoid/v2"
 
 func GenNanoID() (string, error) {
-	return gonanoid.Generate("abcdefghijklmnopqrstuvwxyz", 10)
+	return gonanoid.Generate(alphaLower, 10)
+}
+
+var (
+	alphaLower  string = "abcdefghijklmnopqrstuvwxyz"
+	alphaUpper  string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	number      string = "1234567890"
+	alphaNumber        = alphaLower + alphaUpper + number
+)
+
+func GenNanoIDFromAlphaNumber(size int) (string, error) {
+	return gonanoid.Generate(alphaNumber, size)
 }
