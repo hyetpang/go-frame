5502ef26eac7764d11a94fc644847a9b2c988c00
guangming.zhang <guangming.zhang@yingxiong.com>
2022-08-26 11:42:42 +0800

    🦄 refactor: 优化gin文件代码

--- internal/components/gin/gin.go
+++ internal/components/gin/gin.go
@@ -30,46 +30,7 @@ import (
 )
 
 func New(zapLog *zap.Logger, lc fx.Lifecycle) gin.IRouter {
-	conf := new(config)
-	err := viper.UnmarshalKey("http", conf)
-	if err != nil {
-		logs.Fatal("http配置Unmarshal到对象出错", zap.Error(err))
-	}
-	validate.Must(conf)
-	if len(conf.Addr) < 1 {
-		logs.Fatal("http配置字段addr没有配置值")
-	}
-	router := gin.New()
-	if conf.IsProd {
-		gin.SetMode(gin.ReleaseMode)
-	}
-	router.Use(ginzap.Ginzap(zapLog, time.RFC3339Nano, false))
-	router.Use(recoveryWithZap(zapLog, true))
-	if conf.IsMetrics {
-		m := ginmetrics.GetMonitor()
-		m.SetMetricPath(conf.MetricsPath)
-		m.SetSlowTime(2)
-		m.SetDuration([]float64{0.1, 0.3, 1.2, 5, 10})
-		m.Use(router)
-	}
-	router.NoRoute(noMethod)
-	router.NoMethod(noMethod)
-	// 健康检查
-	router.GET("/health_check", func(ctx *gin.Context) {
-		wrapper.Wrap(ctx).Success("ok")
-	})
-	// 文档
-	if conf.IsDoc {
-		router.GET(conf.DocPath, ginSwagger.WrapHandler(swaggerfiles.Handler))
-	}
-	if conf.IsPprof {
-		// TODO 加权限
-		if len(conf.PprofPrefix) > 0 {
-			pprof.Register(router, conf.PprofPrefix)
-		} else {
-			pprof.Register(router)
-		}
-	}
+	router, conf := newGin(zapLog)
 	lis, err := net.Listen("tcp4", conf.Addr)
 	if err != nil {
 		logs.Fatal("http服务地址监听出错", zap.Error(err), zap.String("监听的地址", conf.Addr))
@@ -116,6 +77,34 @@ func New(zapLog *zap.Logger, lc fx.Lifecycle) gin.IRouter {
 }
 
 func NewWithGraceRestart(zapLog *zap.Logger, state overseer.State, lc fx.Lifecycle) gin.IRouter {
+	router, _ := newGin(zapLog)
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			errC := make(chan error, 1)
+			go func() {
+				if err := router.RunListener(state.Listener); err != nil && err != http.ErrServerClosed {
+					errC <- err
+				}
+			}()
+
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case err := <-errC:
+				return err
+			case <-time.After(time.Second):
+				return nil
+			}
+		},
+		OnStop: func(ctx context.Context) error {
+			logs.Info("http服务器已关闭...")
+			return nil
+		},
+	})
+	return router
+}
+
+func newGin(zapLog *zap.Logger) (*gin.Engine, *config) {
 	conf := new(config)
 	err := viper.UnmarshalKey("http", conf)
 	if err != nil {
@@ -156,40 +145,7 @@ func NewWithGraceRestart(zapLog *zap.Logger, state overseer.State, lc fx.Lifecyc
 			pprof.Register(router)
 		}
 	}
-	lc.Append(fx.Hook{
-		OnStart: func(ctx context.Context) error {
-			errC := make(chan error, 1)
-			go func() {
-				if err := router.RunListener(state.Listener); err != nil && err != http.ErrServerClosed {
-					errC <- err
-				}
-			}()
-
-			select {
-			case <-ctx.Done():
-				return ctx.Err()
-			case err := <-errC:
-				return err
-			case <-time.After(time.Second):
-				// if !conf.IsProd {
-				// 	for _, r := range router.Routes() {
-				// 		logs.Info("注册的路由=>", zap.String("method", r.Method), zap.String("url", r.Path), zap.String("handler", r.Handler))
-				// 	}
-				// }
-				// logs.Info("HTTP服务器启动成功", zap.String("监听地址", state.Listener.Addr().String()))
-				return nil
-			}
-		},
-		OnStop: func(ctx context.Context) error {
-			// err := srv.Shutdown(ctx)
-			// if err != context.Canceled {
-			// 	return err
-			// }
-			logs.Info("http服务器已关闭...")
-			return nil
-		},
-	})
-	return router
+	return router, conf
 }
 
 func noMethod(ctx *gin.Context) {
