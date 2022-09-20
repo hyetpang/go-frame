1ee7690ec7f50cba0da737e5e8c7aeb86cc4bc13
guangming.zhang <guangming.zhang@yingxiong.com>
2022-09-20 17:18:53 +0800

    ✨ feat: 日志增加with方法

--- pkgs/logs/logs.go
+++ pkgs/logs/logs.go
@@ -30,3 +30,44 @@ func Warn(msg string, fields ...zap.Field) {
 func Info(msg string, fields ...zap.Field) {
 	zap.L().Info(msg, fields...)
 }
+
+func With(fields ...zap.Field) *log {
+	// return zap.L().With(fields...).WithOptions(zap.AddCallerSkip())
+	return &log{
+		Logger: zap.L().With(fields...),
+	}
+}
+
+type log struct {
+	*zap.Logger
+}
+
+func (l *log) Error(msg string, fields ...zap.Field) {
+	l.Logger.Error(msg, fields...)
+}
+
+func (l *log) ErrorWithoutNotice(msg string, fields ...zap.Field) {
+	lognotice.Notice(msg, fields...)
+	l.Logger.Error(msg, fields...)
+}
+
+func (l *log) Debug(msg string, fields ...zap.Field) {
+	l.Logger.Debug(msg, fields...)
+}
+
+func (l *log) Fatal(msg string, fields ...zap.Field) {
+	l.Logger.Fatal(msg, fields...)
+}
+
+func (l *log) Warn(msg string, fields ...zap.Field) {
+	l.Logger.Warn(msg, fields...)
+}
+
+func (l *log) Info(msg string, fields ...zap.Field) {
+	l.Logger.Info(msg, fields...)
+}
+
+func (l *log) With(fields ...zap.Field) *log {
+	l.Logger = l.Logger.With(fields...)
+	return l
+}
