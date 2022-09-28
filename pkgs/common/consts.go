bcb4d6ac0383f7f3c1bab3459d2a00f31058fa5e
guangming.zhang <guangming.zhang@yingxiong.com>
2022-09-28 16:18:43 +0800

    ✨ feat: 增加时间格式化常量

--- pkgs/common/consts.go
+++ pkgs/common/consts.go
@@ -27,8 +27,9 @@ const (
 )
 
 const (
-	TimeFormat = "2006-01-02 15:04:05"
-	DateFormat = "2006-01-02"
+	DateTimeFormat = "2006-01-02 15:04:05"
+	DateFormat     = "2006-01-02"
+	TimeFormat     = "15:04:05"
 )
 
 // 默认的数据库连接名字
