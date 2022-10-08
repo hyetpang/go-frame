bc63755b9b3f44733fd587538a45985535fca460
guangming.zhang <guangming.zhang@yingxiong.com>
2022-10-08 15:55:58 +0800

    ✨ feat: 日志对象可导出

--- pkgs/logs/logs.go
+++ pkgs/logs/logs.go
@@ -31,43 +31,42 @@ func Info(msg string, fields ...zap.Field) {
 	zap.L().Info(msg, fields...)
 }
 
-func With(fields ...zap.Field) *log {
-	// return zap.L().With(fields...).WithOptions(zap.AddCallerSkip())
-	return &log{
+func With(fields ...zap.Field) *Log {
+	return &Log{
 		Logger: zap.L().With(fields...),
 	}
 }
 
-type log struct {
+type Log struct {
 	*zap.Logger
 }
 
-func (l *log) Error(msg string, fields ...zap.Field) {
+func (l *Log) Error(msg string, fields ...zap.Field) {
 	lognotice.Notice(msg, fields...)
 	l.Logger.Error(msg, fields...)
 }
 
-func (l *log) ErrorWithoutNotice(msg string, fields ...zap.Field) {
+func (l *Log) ErrorWithoutNotice(msg string, fields ...zap.Field) {
 	l.Logger.Error(msg, fields...)
 }
 
-func (l *log) Debug(msg string, fields ...zap.Field) {
+func (l *Log) Debug(msg string, fields ...zap.Field) {
 	l.Logger.Debug(msg, fields...)
 }
 
-func (l *log) Fatal(msg string, fields ...zap.Field) {
+func (l *Log) Fatal(msg string, fields ...zap.Field) {
 	l.Logger.Fatal(msg, fields...)
 }
 
-func (l *log) Warn(msg string, fields ...zap.Field) {
+func (l *Log) Warn(msg string, fields ...zap.Field) {
 	l.Logger.Warn(msg, fields...)
 }
 
-func (l *log) Info(msg string, fields ...zap.Field) {
+func (l *Log) Info(msg string, fields ...zap.Field) {
 	l.Logger.Info(msg, fields...)
 }
 
-func (l *log) With(fields ...zap.Field) *log {
+func (l *Log) With(fields ...zap.Field) *Log {
 	l.Logger = l.Logger.With(fields...)
 	return l
 }
